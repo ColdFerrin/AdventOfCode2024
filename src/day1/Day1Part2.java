8a79d5247a58b41f45f7737915613ee4598c2476
ColdFerrin <ferrinkatz@ferrinkatz.com>
2024-12-02 10:24:09 -0800

    Day 1

--- src/day1/Day1Part2.java
+++ src/day1/Day1Part2.java
@@ -3,32 +3,38 @@ package day1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class Day1Part1 {
+public class Day1Part2 {
     public static void main(String[] args) throws FileNotFoundException {
 
         ArrayList<Integer> firstNumbers = new ArrayList<>();
-        ArrayList<Integer> secondNumbers = new ArrayList<>();
+        HashMap<Integer, Integer> secondNumbers = new HashMap<>();
 
         Scanner input = new Scanner(new File("/Users/ferrinkatz/IdeaProjects/AdventOfCode24/src/day1/day1.txt"));
         while (input.hasNext()) {
             String line = input.nextLine();
             String[] numbers = line.split(" +");
             firstNumbers.add(Integer.parseInt(numbers[0]));
-            secondNumbers.add(Integer.parseInt(numbers[1]));
-        }
+            int secondNumber = Integer.parseInt(numbers[1]);
 
-        firstNumbers.sort(Integer::compareTo);
-        secondNumbers.sort(Integer::compareTo);
+            if (!secondNumbers.containsKey(secondNumber)) {
+                secondNumbers.put(secondNumber, 1);
+            } else {
+                secondNumbers.put(secondNumber, secondNumbers.get(secondNumber) + 1);
+            }
+        }
 
         int sumDiff = 0;
         for (int i = 0; i < firstNumbers.size(); i++) {
             int firstNumber = firstNumbers.get(i);
-            int secondNumber = secondNumbers.get(i);
-            sumDiff += Math.abs(firstNumber - secondNumber);
+
+            if (secondNumbers.containsKey(firstNumber)) {
+                sumDiff += secondNumbers.get(firstNumber) * firstNumber;
+            }
         }
 
-        System.out.printf("Part 1: %d%n", sumDiff);
+        System.out.printf("Part 2: %d%n", sumDiff);
     }
 }
